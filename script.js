adf645da54a053d6eb7711f74bf4426e523f7329
Sriyansh Srivastava <23f3004255@ds.study.iitm.ac.in>
2025-10-16 14:31:24 +0530

    Round 1 commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.buy-btn').forEach(btn => {
+  btn.addEventListener('click', function() {
+    alert('Thank you for your interest! Checkout is not implemented in this demo.');
+  });
+});
